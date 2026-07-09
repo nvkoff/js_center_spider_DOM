b520362e48bd7e5247298f00d60f78f4d7bb9bca
Mykyta <f1xggofficial@gmail.com>
2026-07-09 11:42:03 +0200

    centered

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+spider.style.position = 'absolute';
+
+spider.style.left = `${wall.offsetWidth / 2 - spider.offsetWidth / 2}px`;
+spider.style.top = `${wall.offsetHeight / 2 - spider.offsetHeight / 2}px`;
