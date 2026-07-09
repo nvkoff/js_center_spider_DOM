3dbc8a7ebcc9248d2c8b15a557ba13db9b22da14
Mykyta <f1xggofficial@gmail.com>
2026-07-09 11:43:06 +0200

    make build

---
+++ index.898e9262.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider");e.style.position="absolute",e.style.left="".concat(t.offsetWidth/2-e.offsetWidth/2,"px"),e.style.top="".concat(t.offsetHeight/2-e.offsetHeight/2,"px");
+//# sourceMappingURL=index.898e9262.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+spider.style.position = "absolute";
+spider.style.left = `${wall.offsetWidth / 2 - spider.offsetWidth / 2}px`;
+spider.style.top = `${wall.offsetHeight / 2 - spider.offsetHeight / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
