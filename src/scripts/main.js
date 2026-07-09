17720ad2aa8643a5be96cf28a07a6908d07f2ba7
Mykyta <f1xggofficial@gmail.com>
2026-07-09 12:04:15 +0200

    solved v3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,5 +5,5 @@ const spider = document.querySelector('.spider');
 
 spider.style.position = 'absolute';
 
-spider.style.left = `${wall.offsetWidth / 2 - spider.offsetWidth / 2}px`;
-spider.style.top = `${wall.offsetHeight / 2 - spider.offsetHeight / 2}px`;
+spider.style.left = `${wall.clientWidth / 2 - spider.offsetWidth / 2}px`;
+spider.style.top = `${wall.clientHeight / 2 - spider.offsetHeight / 2}px`;
